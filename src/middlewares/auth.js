5d2d8524b1d8e8ba97430be51c1eabe950216fb0
ParmS-Musale <musaleparm9541@gmail.com>
2025-01-07 18:03:21 +0530

    Update auth middleware to return a user-friendly error message for missing token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,9 +5,9 @@ const User = require("../models/user");
 const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
-
+    
     if (!token) {
-      throw new Error("Token is not valid..!!");
+      return res.status(401).send("Please Login");
     }
 
     const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
